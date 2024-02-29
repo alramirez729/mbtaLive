7d64d993d0fc2368cf5e76cb002ca9179356fc8a
Lorddread <DERSDtwXt98Tb57>
2024-02-29 13:21:54 -0500

    Initial working editNotes function implemented

--- backend/server/routes/editUserNote.js
+++ backend/server/routes/editUserNote.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const z = require('zod')
-const noteModel = require('../models/noteModel')
-const { newUserValidation } = require('../models/userValidator');
+const z = require('zod');
+const noteModel = require('../models/noteModel');
 const { generateAccessToken } = require('../utilities/generateToken');
 
 router.post('/editUserNote', async (req, res) =>
 {
-    // don't know if i need this
-    const { error } = newUserValidation(req.body);
-    if (error) return res.status(400).send({ message: error.errors[0].message });
-
     const {userId, stationId, note} = req.body
 
-    noteModel.findByIdAndUpdate(userId, {
+    const {userNoteId} = await noteModel.findOne({ userId: userId });
+
+    noteModel.updateOne(userNoteId, 
+    {
         userId : userId, 
         stationId : stationId, 
         note : note
