1f1cf538b12d7a023cfe7f2d5ce25f5019aa53a8
Lorddread <DERSDtwXt98Tb57>
2024-04-02 15:25:10 -0400

    push for work on desktop

--- frontend/src/components/pages/notesPage.js
+++ frontend/src/components/pages/notesPage.js
@@ -10,37 +10,40 @@ import getUserInfo from "../../utilities/decodeJwt";
 const Notes = () => {  
   const [user, setUser] = useState({});
   const [notes, setNotes] = useState({});
-
+  
   
 
+  
   useEffect(() => {
-    setUser(getUserInfo());    
+    setUser(getUserInfo());
     fetchNotes();
   }, []);
-
+  
   const fetchNotes = async () => {
     try {
       const response = await axios.get(`http://localhost:8081/note/byId?userId=${user.username}`);      
-      setNotes(response.data.stationId);
+      setNotes(response.data.stationId)
     } catch (error) {
       console.error("Error fetching notes", error);
     }
   };
   
   if (!user) return (<div><h4>Log in to view this page.</h4></div>)
-  
+    
+  const htmlNotes = notes
   return (
     <div className = "container">
       <div className = "col-md-12 text-center">
         <h1>Notes of {user.username}</h1>        
+        <h1>Notes: {Object.keys(htmlNotes)}</h1>
         <div class="col-md-12 text-center">
-          <ul>            
+                    
           {Object.keys(notes).map(noteId => (
               <li key={noteId} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '10px' }}>                
-                <div>{noteId} : {notes[noteId]}</div>
+                <h2>{noteId} : {notes[noteId]}</h2>
               </li>
             ))}
-          </ul>
+          
         </div>
       </div>
     </div>
