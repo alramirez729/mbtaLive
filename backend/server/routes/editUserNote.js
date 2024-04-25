57ef9e67ff4ee31a9f3c58d9bf2f617b014b4aac
Lorddread <DERSDtwXt98Tb57>
2024-04-25 17:59:27 -0400

    Backend character limits for post and put notes updated

--- backend/server/routes/editUserNote.js
+++ backend/server/routes/editUserNote.js
@@ -4,34 +4,39 @@ const noteModel = require('../models/noteModel');
 
 
 router.put('', async (req, res) => {
-    var {userId ,stationId} = req.body    
+    var {userId ,stationId} = req.body
 
     const userNoteId = await noteModel.findOne({ userId: userId })
+    if (stationId[Object.keys(stationId)[0]]){
+        if (userNoteId){               
 
-    if (userNoteId){               
+            if (Object.keys(userNoteId.stationId).indexOf(Object.keys(stationId)[0]) > -1){
 
-        if (Object.keys(userNoteId.stationId).indexOf(Object.keys(stationId)[0]) > -1){
-
-            Object.assign(userNoteId.stationId,stationId)            
-            
-            noteModel.findByIdAndUpdate(userNoteId._id, 
-            {
-                stationId : userNoteId.stationId
-            } ,function (err, noteInfo) {
-            if (err){
-                console.log(err);
-            } else {
-                res.status(200).send({ message: "Note edited successfully", updatedNote: userNoteId.stationId });
+                Object.assign(userNoteId.stationId,stationId)            
+                
+                noteModel.findByIdAndUpdate(userNoteId._id, 
+                {
+                    stationId : userNoteId.stationId
+                } ,function (err, noteInfo) {
+                if (err){
+                    console.log(err);
+                } else {
+                    res.status(200).send({ message: "Note edited successfully", updatedNote: userNoteId.stationId });
+                }
+                });
             }
-            });
+            else{
+                res.status(404).send({ message: "Error: Note not found." })
+            }
         }
         else{
-            res.status(404).send({ message: "Error: Note not found." })
+            res.status(404).send({ message: "Error: User not found." })
         }
     }
     else{
-        res.status(404).send({ message: "Error: User not found." })
+        res.status(404).send({ message: "Error: blank input." })
     }
+    
 })
 
 
--- backend/server/routes/postUserNote.js
+++ backend/server/routes/postUserNote.js
@@ -8,45 +8,50 @@ router.post('', async (req, res) => {
 
     const userNoteId = await noteModel.findOne({ userId: userId })
 
-    if (userNoteId == null){        
-        const postNote = new noteModel({
-            userId: userId, 
-            stationId: stationId,
-        })    
+    if(stationId[Object.keys(stationId)[0] != ""]){
+        if (userNoteId == null){        
+            const postNote = new noteModel({
+                userId: userId, 
+                stationId: stationId,
+            })    
 
-        try {
-            const postNewNote = await postNote.save();
-            res.send(postNewNote);
-        } catch (error) {
-            res.status(400).send({ message: "Error trying to post new note" });
+            try {
+                const postNewNote = await postNote.save();
+                res.send(postNewNote);
+            } catch (error) {
+                res.status(400).send({ message: "Error trying to post new note" });
+            }
         }
-    }
-    else {        
-        stationArray = Object.keys(userNoteId.stationId)
+        else {        
+            stationArray = Object.keys(userNoteId.stationId)
 
-        if (!stationArray.includes(Object.keys(stationId)[0])){
-            Object.assign(stationId, userNoteId.stationId)
+            if (!stationArray.includes(Object.keys(stationId)[0])){
+                Object.assign(stationId, userNoteId.stationId)
+                
+
+                noteModel.updateOne(userNoteId, 
+                    {
+                        stationId : stationId
+                    } ,function (err, noteInfo) {
+                    if (err){
+                        console.log(err);
+                    }
+                    else{
+                        res.status(200).send({ message: "Successfully posted note" })
+                    }
+                    })        
+            }
+            else{
+                res.status(409).send({ message: "Error: station note already exists, please go to edit." })
+            }        
             
 
-            noteModel.updateOne(userNoteId, 
-                {
-                    stationId : stationId
-                } ,function (err, noteInfo) {
-                if (err){
-                    console.log(err);
-                }
-                else{
-                    res.status(200).send({ message: "Successfully posted note" })
-                }
-                })        
+
+            
         }
-        else{
-            res.status(409).send({ message: "Error: station note already exists, please go to edit." })
-        }        
-        
-
-
-        
+    }
+    else {
+        res.status(404).send({ message: "Error: blank post found." })
     }
 
 })
